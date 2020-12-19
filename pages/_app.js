4f2b564bc84b339f455bed67f6eda18690dff200
8connor <connorh16@gmail.com>
2020-12-18 22:39:31 -0500

    merging

--- pages/_app.js
+++ pages/_app.js
@@ -15,7 +15,7 @@ function MyApp({ Component, pageProps }) {
     router.events.on("routeChangeStart", NProgress.start());
     router.events.on("routeChangeComplete", NProgress.done());
     router.events.on("routeChangeError", NProgress.done());
-  }, [])
+  }, []);
 
   return (
     <>
