c9d7419c94f2af70ebef401c597b3b4598953ab2
The-Napster <connorh16@gmail.com>
2020-12-22 19:55:29 -0500

    updating the server.js for custom https code.

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
-const { createServer } = require('https');
+const https = require('https');
+
 const fs = require('fs');
 const { parse } = require('url');
 const next = require('next');
+const express = require("express");
 
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev, dir: __dirname });
@@ -13,12 +15,29 @@ var options = {
 };
 
 app.prepare().then(() => {
-    createServer(options, (req, res) => {
+    const server = express();
+
+    server.get('*', (req, res) => {
+        return res.redirect( dev ? "https://localhost:3001" : "https://" + req.headers.host + req.url);
+    });
+
+    server.listen(3000, (err) => {
+        if (err) throw err
+        console.log('> Ready on http://localhost:3000')
+    })
+
+    https.createServer(options, (req, res) => {
         const parsedUrl = parse(req.url, true);
 
         handle(req, res, parsedUrl);
-    }).listen(3000, (err) => {
+
+    }).listen(3001, (err) => {
         if (err) throw err;
-        console.log('> Ready on http://localhost:3000');
+        console.log('> Ready on https://localhost:3001');
     });
-});
+})
+    .catch((ex) => {
+        console.error(ex.stack)
+        process.exit(1)
+
+    });
