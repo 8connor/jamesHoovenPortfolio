ad48e491c4ab55a2acfcbdacc1b212e563057d5d
8connor <connorh16@gmail.com>
2021-01-06 11:38:50 -0500

    Working on sidebar compatibility.

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const https = require('https');
-
+const http = require("http");
 const fs = require('fs');
 const { parse } = require('url');
 const next = require('next');
@@ -12,24 +12,24 @@ console.log(dev)
 const app = next({ dev, dir: __dirname });
 const handle = app.getRequestHandler();
 
-var options = {
-    key: fs.readFileSync('ssl.key'),
-    cert: fs.readFileSync('ssl.crt'),
-};
+// var options = {
+//     key: fs.readFileSync('ssl.key'),
+//     cert: fs.readFileSync('ssl.crt'),
+// };
 
 app.prepare().then(() => {
     const server = express();
 
-    server.get('*', (req, res) => {
-        return res.redirect( dev ? "https://localhost:3001" : "https://" + req.headers.host + req.url);
-    });
+    // server.get('*', (req, res) => {
+    //     return res.redirect( dev ? "https://localhost:3001" : "https://" + req.headers.host + req.url);
+    // });
 
-    server.listen(3000, (err) => {
-        if (err) throw err
-        console.log('> Ready on http://localhost:3000')
-    })
+    // server.listen(3000, (err) => {
+    //     if (err) throw err
+    //     console.log('> Ready on http://localhost:3000')
+    // })
 
-    https.createServer(options, (req, res) => {
+    http.createServer( (req, res) => {
         const parsedUrl = parse(req.url, true);
 
         handle(req, res, parsedUrl);
