fcbef8fea29b5a0cd11dd399226fba25b897cf74
8connor <connorh16@gmail.com>
2021-01-11 13:18:31 -0500

    Added a skills section with icons. May need to find a way to make the code more concise in that component.

--- pages/Components/InfoSection/index.js
+++ pages/Components/InfoSection/index.js
@@ -1,11 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Styles from "./InfoSection.module.css";
-import Card from "react-bootstrap/Card";
 import anime from "animejs/lib/anime";
 import PartCon from "../PartCon";
+import AboutSection from "./AboutSection";
 
 function InfoCon() {
   useEffect(() => {
@@ -35,7 +35,7 @@ function InfoCon() {
     };
 
     let observer = new IntersectionObserver(callback, options);
-    
+
     observer.observe(target);
   }, []);
 
@@ -74,37 +74,7 @@ function InfoCon() {
         </Container>
       </PartCon>
 
-      <Container className={`${Styles.aboutCon}`}>
-        <Row>
-          <Col
-            sm={{ span: 12, offset: 0 }}
-            md={{ span: 12, offset: 0 }}
-            lg={{ span: 12, offset: 0 }}
-          >
-            <Card className={`shadow`} id="aboutCard">
-              <Card.Title className="ml-3 h1 text-primary">
-                <h1>About</h1>
-              </Card.Title>
-              <Card.Body>
-                <p>
-                  Full-stack web developer that is focused on making an
-                  enjoyable user experience on the web. I recently graduated
-                  from Vanderbilt University Full-Stack Web Development
-                  Bootcamp, and have acquired many skills relating to web
-                  development. The more notable skills are my ability to work
-                  efficiently with JavaScript, React.js, Node.js, HTML, CSS,
-                  MongoDB, SQL, and JQuery. I focus on making my websites
-                  compatible with mobile responsive designs. I am looking
-                  forward to utilizing my interpersonal skills from a background
-                  in the service industry and my newly acquired skills provide
-                  added-value to an organization through technology and being
-                  detail-oriented.
-                </p>
-              </Card.Body>
-            </Card>
-          </Col>
-        </Row>
-      </Container>
+      <AboutSection />
     </>
   );
 }
