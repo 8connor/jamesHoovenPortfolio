bcc9b8289fbf3a457880e34ec37bec36a8745c7e
8connor <connorh16@gmail.com>
2021-02-17 10:25:02 -0500

    Adding a project.

--- pages/Components/Projects/index.js
+++ pages/Components/Projects/index.js
@@ -132,6 +132,49 @@ function Projects() {
               </Card.Body>
             </Card>
           </Col>
+          <Col
+            sm={{ span: 6, offset: 0 }}
+            md={{ span: 6, offset: 0 }}
+            lg={{ span: 6, offset: 0 }}
+          >
+            <Card
+              className={`${Styles.infoCards} projCards shadow bg-dark text-light`}
+            >
+              <Img
+                src="/images/bugTracker.png"
+                width="400px"
+                height="400px"
+              ></Img>
+              <Card.Body>
+                <Card.Title>bugTracker</Card.Title>
+                <Card.Text>
+                  <p> bugTracker is an application that aims to simplify the process of bug ticket management.</p>
+
+                  <p> This application is currently still under development, but the basic functionality is currently in tact.</p>
+
+                  <p>This application uses React.js, Node.js, MongoDb, Express, and Bootstrap.</p>
+
+                  <p>Test login username: renewi3847@combcub.com password: admin123!</p>
+                  <br />
+                  <br />
+                  <a
+                    className="btn btn-primary mr-3"
+                    href="https://github.com/8connor/bugTracker"
+                    target="_blank"
+                  >
+                    Deployment
+                  </a>
+                  <a
+                    className="btn btn-primary mr-3"
+                    href="https://github.com/8connor/bugTracker"
+                    target="_blank"
+                  >
+                    Github
+                  </a>
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
         </Row>
       </Container>
     </div>
